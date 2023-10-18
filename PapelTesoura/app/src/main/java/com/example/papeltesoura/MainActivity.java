5e111917e476d0780d4f67dabec54aac5af16cb6
IgoorToorres <115846114+IgoorToorres@users.noreply.github.com>
2023-10-18 14:39:34 -0300

    Versão final

--- PapelTesoura/app/src/main/java/com/example/papeltesoura/MainActivity.java
+++ PapelTesoura/app/src/main/java/com/example/papeltesoura/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,17 +27,57 @@ public class MainActivity extends AppCompatActivity {
 
     public void selecionarTesoura(View view){
         verificarResultado("tesoura");
-        resultadoMaquina();
     }
 
-    private void resultadoMaquina(View view){
-        Maquina maquina = new Maquina();
-        ImageView imagemApp = new 
+    private String resultadoMaquina(){
+        ImageView imagemApp = findViewById(R.id.imagem_App);
 
+        int num = new Random().nextInt(3);
+        String[] resultadoMquina = {
+                "pedra",
+                "papel",
+                "tesoura",
+        };
+
+        String escolha = resultadoMquina[num];
+
+        switch (escolha) {
+            case "pedra":
+                imagemApp.setImageResource(R.drawable.pedra);
+                System.out.println("aaaaaaaa");
+                break;
+            case "papel":
+                imagemApp.setImageResource(R.drawable.papel);
+                break;
+            case "tesoura":
+                imagemApp.setImageResource(R.drawable.tesoura);
+                break;
+        }
+        return escolha;
     }
 
     private void verificarResultado( String escolhaUsuario){
-        System.out.println("usuario selecionou: " + escolhaUsuario);
+        TextView resultado = findViewById(R.id.resultado_final);
+        String maquina = resultadoMaquina();
+        if(
+        (maquina == "pedra" && escolhaUsuario == "pedra") ||
+        (maquina == "papel" && escolhaUsuario == "papel")||
+        (maquina == "tesoura" && escolhaUsuario == "tesoura")
+        ){
+            resultado.setText("Empate");
+        }else if(
+        (maquina == "pedra" && escolhaUsuario == "papel") ||
+        (maquina == "papel" && escolhaUsuario == "tesoura") ||
+        (maquina == "tesoura" && escolhaUsuario == "pedra")
+        ){
+            resultado.setText("Você venceu!");
+        }else if(
+        (maquina == "pedra" && escolhaUsuario == "tesoura")||
+        (maquina == "papel" && escolhaUsuario == "pedra")||
+        (maquina == "tesoura" && escolhaUsuario == "papel")
+        ){
+            resultado.setText("Você Perdeu!");
+        }
     }
 
 
--- PapelTesoura/app/src/main/java/com/example/papeltesoura/Maquina.java
+++ PapelTesoura/app/src/main/java/com/example/papeltesoura/Maquina.java
@@ -1,17 +1,1 @@
-package com.example.papeltesoura;
 
-import java.util.Random;
-
-public class Maquina {
-    private int num = new Random().nextInt(3);
-    private String[] resultadoMquina = {
-            "pedra",
-            "papel",
-            "tesoura",
-    };
-
-    public String escolhaMaquina(){
-        return resultadoMquina[num];
-    }
-
-}
