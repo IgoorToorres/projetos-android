81622860702c82ec07a131d020ea1e16273cdd13
IgoorToorres <115846114+IgoorToorres@users.noreply.github.com>
2023-10-24 09:18:25 -0300

    .

--- TelaLoginSenha/app/src/main/java/com/example/telaloginsenha/LoginSenha.java
+++ TelaLoginSenha/app/src/main/java/com/example/telaloginsenha/LoginSenha.java
@@ -3,6 +3,10 @@ package com.example.telaloginsenha;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class LoginSenha extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class LoginSenha extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_senha);
+
+        Button botao = findViewById(R.id.botao_enviar);
+        TextInputEditText email = findViewById(R.id.edit_email);
+        TextInputEditText senha = findViewById(R.id.edit_senha);
+
+        botao.setOnClickListener(v -> {
+            String emailUsuario = email.getText().toString();
+            String senhaUsuario = senha.getText().toString();
+            System.out.println("usuario digitou: " + emailUsuario + " " + senhaUsuario);
+        });
+
     }
 }
