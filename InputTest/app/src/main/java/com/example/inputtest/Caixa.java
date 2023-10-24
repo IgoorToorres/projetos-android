fdd611d949a2ad23327a6ef5173cbfd7b7f88c50
IgoorToorres <115846114+IgoorToorres@users.noreply.github.com>
2023-10-24 08:42:52 -0300

    .

---
+++ InputTest/app/src/main/java/com/example/inputtest/Caixa.java
@@ -0,0 +1,14 @@
+package com.example.inputtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Caixa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_caixa);
+    }
+}
