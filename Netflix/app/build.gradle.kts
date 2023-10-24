35b32305d0ad5a428712d637ade75e41c4918278
IgoorToorres <115846114+IgoorToorres@users.noreply.github.com>
2023-10-24 11:21:11 -0300

    projeto netflix (tela)

--- Netflix/app/build.gradle.kts
+++ Netflix/app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.netflix"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.netflix"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
