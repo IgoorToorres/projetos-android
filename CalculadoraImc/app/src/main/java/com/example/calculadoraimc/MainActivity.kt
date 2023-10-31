c686192bdb2aa654c1e11c0ee479a797c27312d1
IgoorToorres <115846114+IgoorToorres@users.noreply.github.com>
2023-10-31 10:02:59 -0300

    .

--- CalculadoraImc/app/src/main/java/com/example/calculadoraimc/MainActivity.kt
+++ CalculadoraImc/app/src/main/java/com/example/calculadoraimc/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.calculadoraimc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btnCalcular: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnCalcular = findViewById(R.id.btn_calcular)
+        btnCalcular.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+
+            startActivity(intent)
+        }
+
     }
 }
